45470c3df2c602ac3396b4833d10d0b109a6fc55
Ashish Lamichaney <124287828+A5shi5h@users.noreply.github.com>
2025-07-23 16:20:04 +0530

    quick fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import Navbar from '@/components/Navbar';
 import Hero from '@/components/Hero';
 import About from '@/components/About';
 import Projects from '@/components/Projects';
